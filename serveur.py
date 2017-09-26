db3ff882919727d43afbf4949ed05b679c510efa
DumpDos <donvitot@gmail.com>
2017-09-26 18:21:42 +0200

    Update serveur.py

--- serveur.py
+++ serveur.py
@@ -1,43 +1,87 @@
+#!/usr/bin/env python
+
+#--- bibiliotheques ---#
 import os
 import sys
 import time
 import serial
+import fonction
 
-#--- Commandes Hayes ---#
+#--- commandes hayes ---#
 at_ttone = 'ATA0\r\n'
 
-#--- Variables de travail ---#
-cnx = 'null'
-por = 'null'
+#--------------------------------------------------------------------#
+#----------------------------- Script -------------------------------#
+#--------------------------------------------------------------------#
 
-#--- initialisation modem ---#
-execfile("modem_init.py")
+#--- haut de page ---#
+os.system('clear')
 
-time.sleep(2)
-# --- liaison serie --- #
-ser = serial.Serial('/dev/ttyUSB0', 1200, timeout=2)
+print '***************************************************************'
+print '********************* Serveur Minitel *************************'
+print '***************************************************************'
 
-#--- Attente appel ---#
-print 'En ecoute'
-while not 'RING' in cnx:
-	cnx = ser.read(ser.inWaiting())
-	time.sleep(1)
+while True:
+	#--- initialisation des variables ---#
+	tim = 0
+	dcx = 0
+	cnx = ''
+	por = ''
+	din = ''
+	ctl = ''
 
-#--- Envoi porteuse serveur ---#
-print 'Envoi porteuse'
-ser.write(at_ttone)
+	#--- initialisation modem ---#
+	execfile("modem_init.py")
+	time.sleep(0.5)
 
-#--- Detection porteuse minitel ---#
-print 'Connexion en cours'
-while not 'CONN' in por:
-	por = ser.read(ser.inWaiting())
-	time.sleep(1)
+	# --- liaison serie --- #
+	ser = serial.Serial('/dev/ttyUSB0', 1200, timeout=0)
 
-#--- Affichage page acceuil ---#
-print 'Connecte, Affichage page acceuil'
-#os.system('/home/pi/minitel-server/test.sh')
+	#--- attente appel ---#
+	print 'Attente appel'
+	while not 'RING' in cnx:
+		cnx = ser.read(ser.inWaiting())
+		time.sleep(1)
 
-data = sys.stdin.readlines()
-print "Counted", len(data), "lines."
+	#--- envoi porteuse serveur ---#
+	print 'Envoi porteuse'
+	ser.write(at_ttone)
 
-sys.exit(0)
+	#--- detection porteuse minitel ---#
+	print 'Connexion en cours'
+	while not 'CONN' in por:
+		por = ser.read(ser.inWaiting())
+		time.sleep(1)
+
+		#--- timer avant arret ---#
+		if tim > 15:
+			print 'Non connecte'
+			ctl = False
+			break
+
+		else:
+			tim = tim + 1
+
+	#--- verification connexion ---#
+	if ctl != False:
+
+		#--- affichage pages ---#
+		print 'Connecte, Affichage page acceuil'
+		os.system('/home/pi/minitel-server/test.sh') 
+
+		#--- timer avant deconnexion ---#
+		while True:
+			time.sleep(1)
+
+			if dcx > 29:
+				print 'Deconnexion'
+				fonction.deconnexion()
+				time.sleep(3)
+				print 'Reinitialisation'
+				break
+			else:
+				dcx = dcx + 1
+				print dcx
+	else:
+		print 'Reinitialisation'
+#--- fin ---#
